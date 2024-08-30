118e5770b8eb72c6d5b783c149a7c3eb690d7563
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-08-30 18:58:25 -0300

    minimun styling changes

--- src/components/PoemCard.tsx
+++ src/components/PoemCard.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { FaPenFancy } from "react-icons/fa6";
 import "../stylesheets/PoemCard.css";
 export const PoemCard = () => {
   return (
     <>
       <div className="card">
         <div className="header">
+          <span className="icon">
+            <FaPenFancy />
+          </span>
           <p className="alert">New message!</p>
         </div>
 
